49ad9e374604ce8aa3e651efa5fe4ef5275af676
GH818 <1608079002@qq.com>
2021-11-08 20:57:36 +0800

    仿腾讯视频简单web网页

---
+++ static/js/fenye.js
@@ -0,0 +1,13 @@
+function change1(){
+    document.getElementById('myfenye').innerText="1/2";
+}
+function change2(){
+    document.getElementById('myfenye').innerText="2/2";
+}
+
+function change3(){
+    document.getElementById('tongbufen').innerText="1/2";
+}
+function change4(){
+    document.getElementById('tongbufen').innerText="2/2";
+}
